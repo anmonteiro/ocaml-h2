31a908a7d3d67e82abc914942563456c336b76cd
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2019-06-27 10:01:43 -0700

    fix

--- examples/lwt/lwt_get.ml
+++ examples/lwt/lwt_get.ml
@@ -1,21 +1,23 @@
 open H2
 module Client = H2_lwt_unix.Client
 
-let response_handler notify_response_received response response_body =
-  match response.Response.status with
-  | `OK ->
-    let rec read_response () =
-      Body.schedule_read
-        response_body
-        ~on_read:(fun bigstring ~off ~len ->
-          let response_fragment = Bytes.create len in
-          Bigstringaf.blit_to_bytes
-            bigstring
-            ~src_off:off
-            response_fragment
-            ~dst_off:0
-            ~len;
-          print_string (Bytes.to_string response_fragment);
+let response_handler notify_response_received _response response_body =
+  let rec read_response () =
+    Body.schedule_read
+      response_body
+      ~on_eof:(fun () -> Lwt.wakeup_later notify_response_received ())
+      ~on_read:(fun bigstring ~off ~len ->
+        let response_fragment = Bytes.create len in
+        Bigstringaf.blit_to_bytes
+          bigstring
+          ~src_off:off
+          response_fragment
+          ~dst_off:0
+          ~len;
+        print_string (Bytes.to_string response_fragment);
+        read_response ())
+  in
+  read_response ()
 
 let error_handler _ = assert false
 
