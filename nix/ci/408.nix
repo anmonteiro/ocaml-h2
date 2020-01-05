363191fe7ebffb683f219b3d1d8d7cedc726c70f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-04 18:32:04 -0800

    Migrate to Nix & GH Actions (#95)

---
+++ nix/ci/408.nix
@@ -0,0 +1,1 @@
+import ./test.nix { ocamlVersion = "4_08"; }
---
+++ nix/generic.nix
@@ -0,0 +1,52 @@
+{ lib, stdenv, ocamlPackages }:
+
+with ocamlPackages;
+
+let
+  buildH2 = args: buildDunePackage ({
+    version = "0.6.0-dev";
+    doCheck = true;
+    src = lib.cleanSource ./..;
+  } // args);
+
+# TODO: h2-async, h2-mirage
+in rec {
+  hpack = buildH2 {
+    pname = "hpack";
+    buildInputs = [ alcotest hex yojson ];
+    propagatedBuildInputs = [ angstrom faraday ];
+    checkPhase = ''
+      dune build @slowtests -p hpack --no-buffer --force
+    '';
+  };
+
+  h2 = buildH2 {
+    pname = "h2";
+    buildInputs = [ alcotest hex yojson ];
+    propagatedBuildInputs = [
+      angstrom
+      faraday
+      base64
+      psq
+      hpack
+      httpaf
+    ];
+  };
+
+  # These two don't have tests
+  h2-lwt = buildH2 {
+    pname = "h2-lwt";
+    doCheck = false;
+    propagatedBuildInputs = [ h2 lwt4 ];
+  };
+
+  h2-lwt-unix = buildH2 {
+    pname = "h2-lwt-unix";
+    doCheck = false;
+    propagatedBuildInputs = [
+      h2-lwt
+      faraday-lwt-unix
+      lwt_ssl
+    ];
+  };
+}
