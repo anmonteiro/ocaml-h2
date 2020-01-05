1434783f013d8672b7b00b7b8af8d66593aee89f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-04 18:11:52 -0800

    try sleep 3

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -37,19 +37,18 @@ in
     buildInputs = (lib.attrValues h2Drvs) ++ (with ocamlPackages; [ ocaml dune findlib pkgs.ocamlformat ]);
     doCheck = true;
     checkPhase = ''
-      dune exec spec/lwt_h2spec_string.exe
       nohup dune exec spec/lwt_h2spec_string.exe &
-      sleep 2
+      sleep 3
       ${h2spec}/bin/h2spec --strict -p 8080
       kill $(${pkgs.lsof}/bin/lsof -i tcp:8080 | awk '{print $2}' | grep -v PID)
 
       nohup dune exec spec/lwt_h2spec_bigstring.exe &
-      sleep 2
+      sleep 3
       ${h2spec}/bin/h2spec --strict -p 8080
       kill $(${pkgs.lsof}/bin/lsof -i tcp:8080 | awk '{print $2}' | grep -v PID)
 
       nohup dune exec spec/lwt_h2spec_streaming.exe &
-      sleep 2
+      sleep 3
       ${h2spec}/bin/h2spec --strict -p 8080 --timeout 3
       kill $(${pkgs.lsof}/bin/lsof -i tcp:8080 | awk '{print $2}' | grep -v PID)
 
