fe34bcb847b98acf52650708dae8ebbd819ce41e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-05 12:16:56 -0800

    Add `nix-shell` file

---
+++ shell.nix
@@ -0,0 +1,14 @@
+
+let
+  sources = import ./nix/sources.nix {};
+  inherit (sources) pkgs ocamlPackages;
+  inherit (pkgs) stdenv lib;
+  h2Pkgs = pkgs.recurseIntoAttrs (import ./nix { inherit sources; doCheck = false; });
+  h2Drvs = lib.filterAttrs (_: value: lib.isDerivation value) h2Pkgs;
+
+in
+  pkgs.mkShell {
+    inputsFrom = lib.attrValues h2Drvs;
+    buildInputs = with ocamlPackages; [ merlin ];
+  }
+
