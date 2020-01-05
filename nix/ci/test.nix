8b7609fb310321352d39d2ad1284d419ad47f553
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-04 18:15:48 -0800

    build before exec

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -37,18 +37,21 @@ in
     buildInputs = (lib.attrValues h2Drvs) ++ (with ocamlPackages; [ ocaml dune findlib pkgs.ocamlformat ]);
     doCheck = true;
     checkPhase = ''
+      dune build spec/lwt_h2spec_string.exe
       nohup dune exec spec/lwt_h2spec_string.exe &
-      sleep 3
+      sleep 1
       ${h2spec}/bin/h2spec --strict -p 8080
       kill $(${pkgs.lsof}/bin/lsof -i tcp:8080 | awk '{print $2}' | grep -v PID)
 
+      dune build spec/lwt_h2spec_bigstring.exe
       nohup dune exec spec/lwt_h2spec_bigstring.exe &
-      sleep 3
+      sleep 1
       ${h2spec}/bin/h2spec --strict -p 8080
       kill $(${pkgs.lsof}/bin/lsof -i tcp:8080 | awk '{print $2}' | grep -v PID)
 
+      dune build spec/lwt_h2spec_streaming.exe
       nohup dune exec spec/lwt_h2spec_streaming.exe &
-      sleep 3
+      sleep 1
       ${h2spec}/bin/h2spec --strict -p 8080 --timeout 3
       kill $(${pkgs.lsof}/bin/lsof -i tcp:8080 | awk '{print $2}' | grep -v PID)
 
