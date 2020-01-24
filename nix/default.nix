2e77fa99a76d1646a69185855645536c45c15976
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-23 18:22:50 -0800

    Simplify Nix source definition (#102)

--- nix/default.nix
+++ nix/default.nix
@@ -1,9 +1,8 @@
-{ sources ? import ./sources.nix { inherit ocamlVersion; }, ocamlVersion ? "4_09", doCheck ? true }:
+{ pkgs ? import ./sources.nix { inherit ocamlVersion; }
+, ocamlVersion ? "4_09"
+, doCheck ? true }:
 
-let
-  inherit (sources) pkgs ocamlPackages gitignoreSource;
-in
-  pkgs.callPackage ./generic.nix {
-    inherit ocamlPackages gitignoreSource doCheck;
-  }
+pkgs.callPackage ./generic.nix {
+  inherit doCheck;
+}
 
--- nix/sources.nix
+++ nix/sources.nix
@@ -1,23 +1,22 @@
-
-{ pkgs ? import <nixpkgs> {}, ocamlVersion ? "4_09" }:
+# nix-build -E \
+#  'with import <nixpkgs> { overlays = [(import ./.)];}; pkgs.bs-platform'
+{ ocamlVersion ? "4_09" }:
 
 let
-  gitignoreSrc = pkgs.fetchFromGitHub {
-    owner = "hercules-ci";
-    repo = "gitignore";
-    rev = "7415c4f";
-    sha256 = "1zd1ylgkndbb5szji32ivfhwh04mr1sbgrnvbrqpmfb67g2g3r9i";
-  };
-
   overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/d2d883d84.tar.gz;
-    sha256 = "1raq4wnv8h0v23kar79hra6xffkfl281zw4vzvyfr1fwgwwk7bx3";
+    url = https://github.com/anmonteiro/nix-overlays/archive/98b6ee7.tar.gz;
+    sha256 = "12vqdghlz2v7ln28jmrnxy210zv5x21g8kw2s0dvszfmz0p2inkh";
   };
 
-  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-
-  ocamlPackages = pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
-    (pkgs.callPackage "${overlays}/ocaml" { });
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (import overlays)
+      (self: super: {
+        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
+            (super.callPackage "${overlays}/ocaml" {});
+      })
+    ];
+  };
 
 in
-  { inherit pkgs ocamlPackages gitignoreSource; }
+  pkgs
