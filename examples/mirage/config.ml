62baeb5b8f6c45e38ab2ce4ded2cce83580c2bd0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-29 19:28:04 -0700

    fmt

--- examples/mirage/config.ml
+++ examples/mirage/config.ml
@@ -7,15 +7,12 @@ let stack = generic_stackv4 default_network
 (* Dependencies *)
 
 let server =
- let packages =
-   [ package ~pin:"file://../../" "h2-lwt"
-   ; package ~pin:"file://../../" "h2-mirage"
-   ]
+  let packages =
+    [ package ~pin:"file://../../" "h2-lwt"
+    ; package ~pin:"file://../../" "h2-mirage"
+    ]
   in
-  foreign "Unikernel.Make"
-    ~packages
-    (console @-> pclock @-> http2 @-> job)
-
+  foreign "Unikernel.Make" ~packages (console @-> pclock @-> http2 @-> job)
 
 let app = http2_server @@ conduit_direct stack
 
