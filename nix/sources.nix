f5b7c0d9db88468875f578de2b5966a4e8bf59bf
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-16 18:11:57 -0700

    Prep for 0.6.1 release

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/af504f1.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/cd81f24f.tar.gz;
 
 in
 
