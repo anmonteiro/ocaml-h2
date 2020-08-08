ea564f05d22f5fcee94f86afa890f1c609245d44
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-08 10:08:52 -0700

    Use `filterGitSource` from nix-overlays, test on OCaml 4.11 (#139)

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/5cf9c32.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/957c4eb.tar.gz;
 
 in
 
