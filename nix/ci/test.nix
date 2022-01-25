7c85c8012afd6ec08bf69bedc363c2f98693710a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-01-24 20:47:10 -0800

    update

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -1,7 +1,26 @@
 { ocamlVersion }:
 
 let
-  pkgs = import ../sources.nix { inherit ocamlVersion; };
+  lock = builtins.fromJSON (builtins.readFile ./../../flake.lock);
+  src = fetchGit {
+    url = with lock.nodes.nixpkgs.locked;"https://github.com/${owner}/${repo}";
+    inherit (lock.nodes.nixpkgs.locked) rev;
+    # inherit (lock.nodes.nixpkgs.original) ref;
+  };
+  pkgs = import "${src}/boot.nix" {
+    overlays = [
+      (import src)
+      (self: super: {
+        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}";
+
+        pkgsCross.musl64 = super.pkgsCross.musl64 // {
+          ocamlPackages = super.pkgsCross.musl64.ocaml-ng."ocamlPackages_${ocamlVersion}";
+        };
+      })
+    ];
+  };
+
+
   inherit (pkgs) lib stdenv fetchTarball ocamlPackages h2spec;
 
   h2Pkgs = import ./.. { inherit ocamlVersion; };
