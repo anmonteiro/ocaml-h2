dd3a24686e0588f2ad0bcdc728256954bd1792b3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-02-17 23:06:20 -0800

    Fix examples (#160)

--- examples/alpn/lib/http1_handler.ml
+++ examples/alpn/lib/http1_handler.ml
@@ -12,12 +12,12 @@ let redirect_handler : Unix.sockaddr -> Reqd.t Gluten.reqd -> unit =
   Reqd.respond_with_string reqd response ""
 
 let redirect_error_handler
-    :  Unix.sockaddr -> ?request:Request.t -> _
-    -> (Headers.t -> [ `write ] Body.t) -> unit
+    :  Unix.sockaddr -> ?request:Request.t -> _ -> (Headers.t -> Body.Writer.t)
+    -> unit
   =
  fun _client_address ?request:_ _error start_response ->
   let response_body = start_response Headers.empty in
-  Body.close_writer response_body
+  Body.Writer.close response_body
 
 let request_handler : Unix.sockaddr -> Reqd.t Gluten.reqd -> unit =
  fun _client_address { Gluten.reqd; _ } ->
@@ -42,9 +42,9 @@ let request_handler : Unix.sockaddr -> Reqd.t Gluten.reqd -> unit =
   Reqd.respond_with_string reqd response response_body
 
 let error_handler
-    :  Unix.sockaddr -> ?request:Request.t -> _
-    -> (Headers.t -> [ `write ] Body.t) -> unit
+    :  Unix.sockaddr -> ?request:Request.t -> _ -> (Headers.t -> Body.Writer.t)
+    -> unit
   =
  fun _client_address ?request:_ _error start_response ->
   let response_body = start_response Headers.empty in
-  Body.close_writer response_body
+  Body.Writer.close response_body
