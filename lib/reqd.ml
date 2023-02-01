a78428baca8fdcacbdf829a4b9c5100f7202a2f7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-02-01 13:44:48 -0800

    simplify fixed flushing code

--- lib/reqd.ml
+++ lib/reqd.ml
@@ -492,25 +492,21 @@ let flush_response_body t ~max_bytes =
         0
     | Fixed ({ iovec = { buffer; off; len } as iovec; _ } as r)
       when max_bytes > 0 ->
-      if max_bytes < len
-      then (
-        let frame_info =
-          Writer.make_frame_info ~max_frame_size:t.max_frame_size t.id
+      let is_partial_flush = max_bytes < len in
+      let frame_info =
+        let flags =
+          if is_partial_flush
+          then Flags.default_flags
+          else Flags.(set_end_stream default_flags)
         in
-        write_buffer_data t.writer ~off ~len:max_bytes frame_info buffer;
-        r.iovec <- Httpaf.IOVec.shift iovec max_bytes;
-        max_bytes)
-      else
-        let frame_info =
-          Writer.make_frame_info
-            ~max_frame_size:t.max_frame_size
-            ~flags:Flags.(set_end_stream default_flags)
-            t.id
-        in
-        write_buffer_data t.writer ~off ~len frame_info buffer;
-        r.iovec <- Httpaf.IOVec.shift iovec len;
-        close_stream t;
-        len
+
+        Writer.make_frame_info ~max_frame_size:t.max_frame_size ~flags t.id
+      in
+      let len_to_write = if is_partial_flush then max_bytes else len in
+      write_buffer_data t.writer ~off ~len:len_to_write frame_info buffer;
+      r.iovec <- Httpaf.IOVec.shift iovec len_to_write;
+      if not is_partial_flush then close_stream t;
+      len_to_write
     | Fixed _ | Waiting | Complete _ -> 0)
   | _ -> 0
 
