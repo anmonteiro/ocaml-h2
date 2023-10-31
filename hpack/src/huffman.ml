a867aa65a45894392f0a659d5637cbf911c3b0a6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-10-30 19:14:18 -0700

    fix(hpack): huffman coding for codes > 24 bit

--- hpack/src/huffman.ml
+++ hpack/src/huffman.ml
@@ -48,69 +48,53 @@ let encoded_length s =
   in
   loop 0 0
 
-let drain t bits len =
-  let rec loop bits len =
-    if len >= 8
-    then (
-      let len = len - 8 in
-      Faraday.write_uint8 t (bits lsr len);
-      let bits = bits land ((1 lsl len) - 1) in
-      loop bits len)
-    else len, bits
-  in
-  loop bits len
-
 let encode t s =
-  let len = String.length s in
-  let rec loop i bits n =
-    if i < len
-    then
-      let input = Char.code s.[i] in
-      let code, len_in_bits = Huffman_table.encode_table.(input) in
-      let n, bits =
-        if len_in_bits > 24
-        then
-          let bits =
-            (bits lsl (len_in_bits - 24)) lor (code lsr (len_in_bits - 25))
-          in
-          drain t bits (n + len_in_bits - 24)
-        else n, bits
-      in
-      let n', bits' =
-        drain t ((bits lsl len_in_bits) lor code) (n + len_in_bits)
-      in
-      loop (i + 1) bits' n'
-    else n, bits
+  let bits = ref 0 in
+  let bits_left = ref 40 in
+  for i = 0 to String.length s - 1 do
+    let code, code_len = Huffman_table.encode_table.(Char.code s.[i]) in
+    bits_left := !bits_left - code_len;
+    bits := !bits lor (code lsl !bits_left);
+    while !bits_left <= 32 do
+      Faraday.write_uint8 t (!bits lsr 32);
+      bits := !bits lsl 8;
+      bits_left := !bits_left + 8
+    done
+  done;
+  if !bits_left < 40
+  then (
+    bits := !bits lor ((1 lsl !bits_left) - 1);
+    (* add EOS and padding *)
+    Faraday.write_uint8 t (!bits lsr 32))
+
+let decode =
+  let[@inline] add_output buffer c =
+    if c <> '\000' then Buffer.add_char buffer c
   in
-  let n, bits = loop 0 0 0 in
-  if n > 0 then ignore (drain t ((bits lsl 7) lor 0x7f) (n + 7))
-
-let[@inline] add_output buffer c = if c <> '\000' then Buffer.add_char buffer c
-let[@inline] exists_in_huffman_table token = token <> -1
-
-let decode s =
-  let len = String.length s in
-  let buffer = Buffer.create len in
-  let rec loop id accept i =
-    if i < len
-    then (
-      let input = Char.code s.[i] in
-      let index = (id lsl 4) + (input lsr 4) in
-      let id, _, output = Huffman_table.decode_table.(index) in
-      add_output buffer output;
-      if exists_in_huffman_table id
+  let[@inline] exists_in_huffman_table token = token <> -1 in
+  fun s ->
+    let len = String.length s in
+    let buffer = Buffer.create len in
+    let rec loop id accept i =
+      if i < len
       then (
-        let index = (id lsl 4) + (input land 0x0f) in
-        let id, accept, output = Huffman_table.decode_table.(index) in
+        let input = Char.code s.[i] in
+        let index = (id lsl 4) + (input lsr 4) in
+        let id, _, output = Huffman_table.decode_table.(index) in
         add_output buffer output;
         if exists_in_huffman_table id
-        then loop id accept (i + 1)
+        then (
+          let index = (id lsl 4) + (input land 0x0f) in
+          let id, accept, output = Huffman_table.decode_table.(index) in
+          add_output buffer output;
+          if exists_in_huffman_table id
+          then loop id accept (i + 1)
+          else Error Decoding_error)
         else Error Decoding_error)
-      else Error Decoding_error)
-    else if not accept
-    then Error Decoding_error
-    else Ok ()
-  in
-  match loop 0 true 0 with
-  | Ok _ -> Ok (Buffer.contents buffer)
-  | Error e -> Error e
+      else if not accept
+      then Error Decoding_error
+      else Ok ()
+    in
+    match loop 0 true 0 with
+    | Ok _ -> Ok (Buffer.contents buffer)
+    | Error e -> Error e
