94c0aeb30fd13811560cadfb36cf2ba6151d4fa6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-08-26 23:29:46 -0700

    don't run eio on 4.14

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -90,17 +90,19 @@ stdenv.mkDerivation {
     kill $(lsof -i tcp:8080 -t)
 
     # Run Eio h2spec now
-    dune exec --display=short spec/eio_h2spec.exe &
-    while [ -z "$(lsof -t -i tcp:8080)" ]; do
-      sleep 1;
-    done;
+    ${if lib.versionOlder "5.0" ocaml.version then ''
+      dune exec --display=short spec/eio_h2spec.exe &
+      while [ -z "$(lsof -t -i tcp:8080)" ]; do
+        sleep 1;
+      done;
 
-    h2spec --strict -p 8080 -P /string
+      h2spec --strict -p 8080 -P /string
 
-    h2spec --strict -p 8080 -P /bigstring
+      h2spec --strict -p 8080 -P /bigstring
 
-    h2spec --strict -p 8080 --timeout 3 -P /streaming
+      h2spec --strict -p 8080 --timeout 3 -P /streaming
 
-    kill $(lsof -i tcp:8080 -t)
+      kill $(lsof -i tcp:8080 -t)
+    '' else ""}
   '';
 }
