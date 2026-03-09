778e99d4f59cc02bd8ebae67615781550349dbde
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-03-08 20:31:23 -0700

    add more ci versions (#263)
    * add more ci versions
    
    * fix

--- flake.nix
+++ flake.nix
@@ -6,24 +6,38 @@
   inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
   inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nix-filter,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages_5_2.overrideScope (oself: osuper: {
-            gluten-lwt-unix = osuper.gluten-lwt-unix.overrideAttrs (o: {
-              propagatedBuildInputs = o.propagatedBuildInputs ++ [ oself.tls-lwt ];
-            });
-          });
-        });
+        pkgs = nixpkgs.legacyPackages."${system}".extend (
+          self: super: {
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_5.overrideScope (
+              oself: osuper: {
+                gluten-lwt-unix = osuper.gluten-lwt-unix.overrideAttrs (o: {
+                  propagatedBuildInputs = o.propagatedBuildInputs ++ [ oself.tls-lwt ];
+                });
+              }
+            );
+          }
+        );
         packages = pkgs.callPackage ./nix { nix-filter = nix-filter.lib; };
       in
       {
-        packages = packages // { default = packages.h2; };
+        packages = packages // {
+          default = packages.h2;
+        };
         defaultPackage = packages.h2;
         devShells = {
           default = pkgs.callPackage ./nix/shell.nix { inherit packages; };
           release = self.devShells.${system}.default.override { release-mode = true; };
         };
-      });
+      }
+    );
 }
